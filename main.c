665fa18f34aab7b9b1f657d7a9c1d839128697d1
christian le touzo <christian.le-touzo@epita.fr>
2022-06-16 17:44:05 +0200

    library that will be used to the project in order to be connected to WIFI as well as set up the MQTT server for the BOX

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "espMQTTClient/src/ESP32HTTPUpdateServer.h"
+#include "WiFi/src/WiFi.h"
+#include "WiFi/src/WiFiClient.h"
+#include "WiFi/src/WiFiServer.h"
+
+
+void wifi_init()
+{
+	printf("Setting Wifi...");
+	return;
+}
+
+void server_MQTT()
+{
+	printf("Setting MQTT server...");
+	return;
+}
+
+
